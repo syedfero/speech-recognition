9fc32f5eca5232297ec135eb173f96ae51ea8489
syedfero <168407783+syedfero@users.noreply.github.com>
2024-04-29 14:41:18 +0530

    Add files via upload

---
+++ task.cs
@@ -0,0 +1,42 @@
+*{
+    padding: 0;
+    margin: 5;
+    box-sizing: border-box;
+}
+
+body {
+    background: #85C1E9;
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+}
+
+textarea {
+    font-family: Sans-Serif;
+    padding: 15px 20px;
+    text-align: justify;
+    border: 0;
+    outline: 0;
+    color: red;
+    box-shadow: inset 2px 3px 5px grey;
+    border-radius: 8px;
+}
+
+button {
+    margin-top: 8px;
+    padding: 10px 20px;
+    border: 0;
+    outline: 0;
+    background: #CD6155;
+    color: white;
+    border-radius: 8px;
+}
+
+h1 {
+    color: white;
+    font-family: Sans-Serif;
+    margin-bottom: 10px;
+    text-shadow: 2px 3px 5px grey;
+    letter-spacing: 2px;
+}
